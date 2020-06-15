90c8d5cba9f7a1eeff63a710c570f753792ac11d
JKbin <wiken0072@naver.com>
2020-06-15 17:45:31 +0900

    form 로드 and form 활성화

--- ClockApp/Form1.cs
+++ ClockApp/Form1.cs
@@ -30,6 +30,19 @@ namespace ClockApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MessageBox.Show("Form 로드시 발생");
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Form 클로즈시 발생");
+
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 활성화시 발생");
 
         }
     }
